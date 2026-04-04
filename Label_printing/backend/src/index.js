fccc7dfda0dba21b6a9090ff9b080293572bad54
harilakshminarayana1106 <harilakshminarayana.jerobytes@gmail.com>
2026-04-04 13:20:40 +0530

    added corsa

--- Label_printing/backend/src/index.js
+++ Label_printing/backend/src/index.js
@@ -27,7 +27,8 @@ const app = express();
 // ============================================
 
 // 1. CORS (must be first)
-app.use(cors(corsOptions));
+app.use(cors);
+//app.use(cors(corsOptions));
 
 // 2. Body Parser
 app.use(express.json({ limit: '10mb' }));
